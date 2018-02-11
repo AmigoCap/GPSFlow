c835c7b45a93035e00960c833b4aae804b62e4a3
Alexandros Popov <alexandros.popov@ecl14.ec-lyon.fr>
2018-02-11 12:13:08 +0100

    Added file projectColors.py.

---
+++ projectColors.py
@@ -0,0 +1,24 @@
+def defineColorsList():
+
+    return [
+        'navy',
+        'gold',
+        'tomato',
+        'darkolivegreen',
+        'firebrick',
+        'mediumspringgreen',
+        'deeppink',
+        'dimgray',
+        'darkorange',
+        'lightblue',
+        'chocolate',
+        'mediumpurple',
+        'limegreen',
+        'thistle',
+        'darkcyan',
+        'khaki',
+        'aqua',
+        'darkorchid',
+        'sandybrown',
+        'violet'
+    ]
